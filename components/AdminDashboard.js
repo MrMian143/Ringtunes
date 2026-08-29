4c0ae13f288627f4c9d173f36aaaa7caf61bd0e3
MrMian143 <zaidsarwar1403@gmail.com>
2026-08-29 09:42:17 +0500

    Add files via upload

--- components/AdminDashboard.js
+++ components/AdminDashboard.js
@@ -40,6 +40,7 @@ export default function AdminDashboard({ initialRingtones }) {
         access: "public",
         handleUploadUrl: "/api/admin/upload",
         abortSignal: controller.signal,
+        multipart: true,
         onUploadProgress: (event) => setProgress(Math.round(event.percentage)),
       });
 
